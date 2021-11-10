987fcb99f5888ed6fbc57e0d94be1573077ec142
Tommaso van Gelderen <tommasovangelderen@gmail.com>
2021-11-10 18:13:20 +0100

    Achtergrond LogInPagina Gemaakt

---
+++ src/LogInPagina.java
@@ -0,0 +1,12 @@
+import keeptoo.KGradientPanel;
+
+import javax.swing.*;
+
+public class LogInPagina {
+    private JPanel panel1;
+    private KGradientPanel KGradientPanel1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
