947e240402bf16d74ac19e86980949b34039fd22
raven-music <rafael@kruiniger.com>
2021-11-25 17:06:52 +0100

    los apparaat fix
    volgens mij werkt het nu

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -149,5 +149,40 @@ public class MainProgram {
         };
 
     }
+    public static void losapparaatScherm () {
+
+
+        LosApparaat losApparaat = new LosApparaat();
+        losApparaat.setVisible(true);
+
+        losApparaat.callback = (losApparaatReturn) -> {
+
+            //1 is logout
+            //2 is home
+            //3 is apparaten
+            if (losApparaatReturn == 1){
+                losApparaat.setVisible(false);
+                try {
+                    loginScherm();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (losApparaatReturn == 2){
+                losApparaat.setVisible(false);
+                //homeScherm();
+            }
+
+            if (losApparaatReturn == 3){
+                losApparaat.setVisible(false);
+                apparatenScherm();
+            }
+
+        };
+
+
+
+    }
 
 }
