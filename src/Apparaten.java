f965f3c3c31258e2cac6e6ea52d0c7345a063db3
Mohammedirezzi <93650621+Mohammedirezzi@users.noreply.github.com>
2021-12-15 10:42:46 +0100

    Update Apparaten.java

--- src/Apparaten.java
+++ src/Apparaten.java
@@ -184,18 +184,23 @@ public class Apparaten extends JFrame {
 
         }
 
-
         public class Apparaat extends JFrame {
 
-        public JButton dag = new JButton("dag");
+            public JButton dag = new JButton("dag");
 
-        public JButton week = new JButton("week");
+            public JButton week = new JButton("week");
 
-        public JButton maand = new JButton("maand");
+            public JButton maand = new JButton("maand");
+
+            public Consumer<Integer> callback;
+
+            private void ButtonPressed(ActionEvent e) {
+                if (e.getSource() == dag) callback.accept(1);
+                if (e.getSource() == week) callback.accept(2);
+                if (e.getSource() == maand) callback.accept(3);
+            }
 
 
 
-    }
 
-
-}
+        }
